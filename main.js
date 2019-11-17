6387357be22c5b89812c7b836ab9a4c46a3425e9
Souissi Alaa <54039072+SouissiAlaa12@users.noreply.github.com>
2019-11-17 14:14:46 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function bold()
+{
+document.getElementById("modi").style.fontWeight="bold" ; 
+}
+function italic()
+{
+    document.getElementById("modi").style.fontStyle="italic" ; 
+}
+function under()
+{
+    document.getElementById("modi").style.textDecoration="underline" ;
+}
+function selection()
+{
+    document.getElementById("modi").style.fontSize=document.getElementById("selection").value ;
+ 
+}
+function police()
+{
+    document.getElementById("modi").style.fontFamily=document.getElementById("font").value ;
+ 
+}
